71803966516ba38c2c967a176bb2c0d7fbc2d050
Jason <2862304720@qq.com>
2026-05-12 22:45:28 +0800

    学习了digital tree 里面的 插入键值对算法

--- The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying/DigitalTree.java
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying/DigitalTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DigitalTree{
     /*
         Let A be an alphabet (finite set of symbols)
@@ -37,4 +39,37 @@ public class DigitalTree{
         - 唯一的例外是根节点（root）
         - 当树为空时，根节点没有子节点也没有 value，这是允许的
     */
+
+
+
+
+    private void InsertionAlgorithm(){
+        /*
+            To insert a key a0,...,am-1 with value v into a trie :
+            
+            1. Start at the root of the trie (从根节点开始)
+
+            2. For each i from 0 to m-1 :
+                Go down to the child corresponding to ai (根据当前字符 ai 选择对应的子节点)
+                (if the child does not exist, create a new node) (如果子节点不存在，创建一个新节点)
+
+            3. Mark the node to indicate that the key is present. (标记该节点表示这个 key 存在)
+               Store the value v in the node. (在该节点存储 value v)
+        */
+    }
+
+
+}
+
+
+class TrieNode<V>{
+    boolean hasKey; // 是否是一个有效 key 的终点
+    V value; // 存储与 key 相关联的值
+    ArrayList<TrieNode<V>> children; // 孩子节点数组，大小为 |A|
+
+    public TrieNode(){
+        this.hasKey = false; // 默认不是一个有效 key 的终点
+        this.value = null; // 默认没有关联的值
+        this.children = new ArrayList<>(); // 初始化孩子节点数组
+    }
 }
