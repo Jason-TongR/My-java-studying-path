b8df2ecc08a59389a8d98123d29328a14909260e
Jason-TongR <tang25911@gtiit.edu.cn>
2026-06-14 20:48:49 +0800

    Add Bellman-Ford and Floyd-Warshall algorithm descriptions to Shortest_path_in_graphs class

--- The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Shortest_path_in_graphs.java
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Shortest_path_in_graphs.java
@@ -26,5 +26,63 @@ public class Shortest_path_in_graphs {
                 }
             }
             ---------------------------------------------------------------------------------------------------------------------------------------------
+        
+
+
+
+        Bellman-Ford algorithm :
+            We have a directed graph G = (V,E) and possibly negative weight to each edge.
+            Dijkstra's algorithm is no correct here.
+
+            Input : a diagraph G with weights and a vertex v in V.
+            Precondition : the graph has no negative cycles
+            Output : a distance verctor dist , where dist[w] is the weight of the shortest path from v to w , or +00 if there is no such path.
+
+            ---------------------------------------------------------------------------------------------------------------------------------------------
+            Initialize the distance vector with dist[v] := 0 and dist[x] := +00 for every other vertex x != v
+
+            Repeat n times {
+                For each (x -> y) in E {
+                    Let w be the weight of the edge x -> y.
+                    Update the distance of y setting dist[y] = min{dist[y],dist[x] + w}
+                }
+            }
+            -----------------------------------------------------------------------------------------------------------------------------------------------
+            Worst case time complexity : O(n*m)
+
+
+            Question :
+                1. Can the algorithm be adapted to work with undirected graphs?                                 No , if yes , then <--> with negative weight is a negative cycle 
+                2. Can the algorithm be adapted to detect if there are cycles of negative weight?               Yes
+                
+                
+
+
+        Floyd-Warshall algorithm :
+            Let G = (V,E) be a directed graph with posiibly negative weights.
+            We want to find the distance between every pair of vertices. i.e. dist[x][y] is the weight of the short path from x to y.
+            We could use Bellman-Ford n times , Worst case of it is O(n^4) if the graph is dense. but we can do better.
+
+
+
+            Input : a digraph G = (V,E) with weights.
+            Precondition : the graph has no negative cycles.
+            Output : a distance vector dist , where dist[v][w] is the weight of the shortest path from v to w or +00 if there is no such path.
+
+            ---------------------------------------------------------------------------------------------------------------------------------------------------------
+            Initialize the distance vector with dist[x][x] := 0 and for every x != y :
+                dist[x][y] := w (if there is an edge x -> y of weight w) , (+00 otherwise)
+
+            For each z in V {
+                For each x in V{
+                    for each y in V{
+                        Update the distance from x to y , setting dist[x][y] := min{dist[x][y] , dist[x][z]+dist[z][y]}
+                    }
+                }
+            }
+            ----------------------------------------------------------------------------------------------------------------------------------------------------------
+            Worst-case time complexity : O(n^3)
+
+            Note : the nesting (z,x,y) is improtant . E.g. x,y,z does not work.
     */
 }
