51540579b11888e16c0fc20ec7aa6605893bc713
Jason-TongR <tang25911@gtiit.edu.cn>
2026-06-14 12:21:23 +0800

    Add Dynamic_programming class with initial implementation and comments on dynamic programming concepts

---
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Dynamic_programming.java
@@ -0,0 +1,22 @@
+public class Dynamic_programming {
+    /*
+        Dynamic programming is an algorithm desin technique.
+
+        avoiding repeated computations
+
+        设 P 是要解决的原始问题。
+
+        设 P′ 是 P 的一个子问题（规模更小的同类问题）。
+
+        设 S 是 P 的一个最优解（全局最优）。
+
+        如果 S 中对应于 P′ 的那部分子解 S′，恰好也是子问题 P′ 的最优解，
+        那么就说问题 P 具有 最优子结构性质。
+
+        “大问题的最优解，里面一定包含着各个小问题的最优解。”
+
+
+
+        Dynamic programming : minimum weight path in a matrix
+    */
+}
