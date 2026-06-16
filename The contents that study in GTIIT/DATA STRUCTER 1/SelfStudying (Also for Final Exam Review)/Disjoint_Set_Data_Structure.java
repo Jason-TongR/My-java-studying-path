85f5b071355e0128f57f3cd10b5a7bba5a9538f2
Jason-TongR <tang25911@gtiit.edu.cn>
2026-06-16 15:03:32 +0800

    Add find and merg for Disjoint_Set_Data_Structure class

--- The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Disjoint_Set_Data_Structure.java
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Disjoint_Set_Data_Structure.java
@@ -11,5 +11,62 @@ public class Disjoint_Set_Data_Structure {
             Make a new set with a single element.
             Merge two sets , replacing them by their union.
             Find the representative of the set of an element.
+            
+
+
+        Merge-find data structure : 
+            We store an ara p such that p[i] is the parent of i : 
+                If is the representative of a set , P[i] = i.
+                Otherewise , p[i] is closer to the representative.
+        
+                
+
+        ----------------------------------------------------------------------------------------------
+        Optimized find (with path compression)
+            int find(int i) {
+                if (P[i] == i){
+                    return i;
+                }
+                else{
+                    int j = find(P[i]);
+                    P[i] = j;       ---this is the path compression
+                    return j;    
+                }
+            }
+        ----------------------------------------------------------------------------------------------
+
+
+
+        Optimizing merge 
+            Merging by rank :
+                store a number of each element , called its rank.
+                When merging , the node with largerst tank becomes the root.
+                The rank is like the height of the tree , but it is not kept up to date by path compression.
+
+                void merge_by_rank(int i  , int j){
+                    int rep_i = find(i);
+                    int rep_j = find(j);
+                    
+                    if(rep_i == rep_j){
+                        return;
+                    }
+
+                    if(rank[rep_i] > rank[rep_j]){
+                        P[rep_j] = rep_i;
+                    }
+                    else{
+                        p[rep_i] = rep_j;
+
+                        if (rank[rep_i] == rank[rep_j]){
+                            rank[rep_j]++;
+                        }
+                    }
+                }
+            
+
+
+            (Another possible way for merging)
+            Merging by size(the number of the node of the tree)
+                The node with most descendants becomes te new root.
     */
 }
