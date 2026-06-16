d55fe7e858c421a90d55dcf13d4747d80f218d05
Jason-TongR <tang25911@gtiit.edu.cn>
2026-06-16 10:20:49 +0800

    Add Disjoint_Set_Data_Structure class with initial implementation and comments on set operations

---
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Disjoint_Set_Data_Structure.java
@@ -0,0 +1,15 @@
+public class Disjoint_Set_Data_Structure {
+    /*
+        we want to represent a family of sets {A1,A2,...,Ak} where:
+            Each set Ai is finite and non-empty.
+            Each set Ai has a special element called its representative.
+            The sets are pairwise disjoint : i != j ==> Ai intersection Aj = empty set.
+            (This is useful to work with partitions and equivalence relations)
+        
+
+        We need the following operations:
+            Make a new set with a single element.
+            Merge two sets , replacing them by their union.
+            Find the representative of the set of an element.
+    */
+}
