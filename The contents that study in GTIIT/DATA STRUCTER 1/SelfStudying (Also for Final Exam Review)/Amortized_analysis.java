175314c0bd59e38542f87578284449415419319d
Jason-TongR <tang25911@gtiit.edu.cn>
2026-06-17 11:58:52 +0800

    Add Amortized_analysis class with documentation on amortized and aggregate analysis

---
+++ The contents that study in GTIIT/DATA STRUCTER 1/SelfStudying (Also for Final Exam Review)/Amortized_analysis.java
@@ -0,0 +1,17 @@
+public class Amortized_analysis {
+    /*
+        Amortized analysis :
+            Amortized analysis is a way to assign costs to operations ,
+            when they are part of a bigger sequence of operations.
+        
+        
+        
+        Aggreate analysis : (聚合分析) (平均值法)
+            The simplest form of amortized analysis is aggregate analysis.
+            The cost of each operations is the averge cost of all operations.
+
+            Example : 
+                1. Queues with removeN      
+                2. Incrementng binary numbers
+    */
+}
